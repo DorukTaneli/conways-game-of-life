f4ca34655a3d14b5dee427a490eddb14a451090e
DorukTaneli <dtaneli16@ku.edu.tr>
2020-02-28 23:20:20 +0300

    format code

--- life.c
+++ life.c
@@ -14,6 +14,7 @@
 #include <math.h>
 #include <assert.h>
 #include <string.h>
+#include <omp.h>
 
 #define MATCH(s) (!strcmp(argv[ac], (s)))
 
@@ -22,12 +23,12 @@ int MeshPlot(int t, int m, int n, char **mesh);
 double real_rand();
 int seed_rand(long sd);
 
-static char **currWorld=NULL, **nextWorld=NULL, **tmesh=NULL;
-static int maxiter = 200; /* number of iteration timesteps */
+static char **currWorld = NULL, **nextWorld = NULL, **tmesh = NULL;
+static int maxiter = 200;  /* number of iteration timesteps */
 static int population = 0; /* number of live cells */
 
-int nx = 100;      /* number of mesh points in the x dimension */
-int ny = 100;      /* number of mesh points in the y dimension */
+int nx = 100; /* number of mesh points in the x dimension */
+int ny = 100; /* number of mesh points in the y dimension */
 
 //static int w_update = 0;
 //static int w_plot = 1;
@@ -35,150 +36,181 @@ int ny = 100;      /* number of mesh points in the y dimension */
 double getTime();
 extern FILE *gnu;
 
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
-    int i,j,ac;
+    int i, j, ac;
 
     /* Set default input parameters */
-    
-    float prob = 0.5;   /* Probability of placing a cell */
+
+    float prob = 0.5; /* Probability of placing a cell */
     long seedVal = 0;
     int game = 0;
     int s_step = 0;
     int numthreads = 1;
-    int disable_display= 0; 
+    int disable_display = 0;
 
     /* Over-ride with command-line input parameters (if any) */
-    
-    for(ac=1;ac<argc;ac++)
+
+    for (ac = 1; ac < argc; ac++)
     {
-        if(MATCH("-n")) {nx = atoi(argv[++ac]);}
-        else if(MATCH("-i")) {maxiter = atoi(argv[++ac]);}
-        else if(MATCH("-t"))  {numthreads = atof(argv[++ac]);}
-        else if(MATCH("-p"))  {prob = atof(argv[++ac]);}
-        else if(MATCH("-s"))  {seedVal = atof(argv[++ac]);}
-        else if(MATCH("-step"))  {s_step = 1;}
-        else if(MATCH("-d"))  {disable_display = 1;}
-        else if(MATCH("-g"))  {game = atoi(argv[++ac]);}
-        else {
-            printf("Usage: %s [-n < meshpoints>] [-i <iterations>] [-s seed] [-p prob] [-t numthreads] [-step] [-g <game #>] [-d]\n",argv[0]);
-            return(-1);
+        if (MATCH("-n"))
+        {
+            nx = atoi(argv[++ac]);
+        }
+        else if (MATCH("-i"))
+        {
+            maxiter = atoi(argv[++ac]);
+        }
+        else if (MATCH("-t"))
+        {
+            numthreads = atof(argv[++ac]);
+        }
+        else if (MATCH("-p"))
+        {
+            prob = atof(argv[++ac]);
+        }
+        else if (MATCH("-s"))
+        {
+            seedVal = atof(argv[++ac]);
+        }
+        else if (MATCH("-step"))
+        {
+            s_step = 1;
+        }
+        else if (MATCH("-d"))
+        {
+            disable_display = 1;
+        }
+        else if (MATCH("-g"))
+        {
+            game = atoi(argv[++ac]);
+        }
+        else
+        {
+            printf("Usage: %s [-n < meshpoints>] [-i <iterations>] [-s seed] [-p prob] [-t numthreads] [-step] [-g <game #>] [-d]\n", argv[0]);
+            return (-1);
         }
     }
 
     int rs = seed_rand(seedVal);
     /* Increment sizes to account for boundary ghost cells */
-    
-    nx = nx+2;
-    ny = nx; 
-    
+
+    nx = nx + 2;
+    ny = nx;
+
     /* Allocate contiguous memory for two 2D arrays of size nx*ny.
      * Two arrays are required because in-place updates are not
      * possible with the simple iterative scheme listed below */
-    
-    currWorld = (char**)malloc(sizeof(char*)*nx + sizeof(char)*nx*ny);
-    for(i=0;i<nx;i++) 
-      currWorld[i] = (char*)(currWorld+nx) + i*ny;
-    
-    nextWorld = (char**)malloc(sizeof(char*)*nx + sizeof(char)*nx*ny);
-    for(i=0;i<nx;i++) 
-      nextWorld[i] = (char*)(nextWorld+nx) + i*ny;
-    
+
+    currWorld = (char **)malloc(sizeof(char *) * nx + sizeof(char) * nx * ny);
+    for (i = 0; i < nx; i++)
+        currWorld[i] = (char *)(currWorld + nx) + i * ny;
+
+    nextWorld = (char **)malloc(sizeof(char *) * nx + sizeof(char) * nx * ny);
+    for (i = 0; i < nx; i++)
+        nextWorld[i] = (char *)(nextWorld + nx) + i * ny;
+
     /* Set the boundary ghost cells to hold 'zero' */
-    for(i=0;i<nx;i++)
+    for (i = 0; i < nx; i++)
     {
-        currWorld[i][0]=0;
-        currWorld[i][ny-1]=0;
-        nextWorld[i][0]=0;
-        nextWorld[i][ny-1]=0;
+        currWorld[i][0] = 0;
+        currWorld[i][ny - 1] = 0;
+        nextWorld[i][0] = 0;
+        nextWorld[i][ny - 1] = 0;
     }
-    for(i=0;i<ny;i++)
+    for (i = 0; i < ny; i++)
     {
-        currWorld[0][i]=0;
-        currWorld[nx-1][i]=0;
-        nextWorld[0][i]=0;
-        nextWorld[nx-1][i]=0;
+        currWorld[0][i] = 0;
+        currWorld[nx - 1][i] = 0;
+        nextWorld[0][i] = 0;
+        nextWorld[nx - 1][i] = 0;
     }
 
     // Generate a world
-    
-    if (game == 0){ // Use Random input
-        for(i=1;i<nx-1;i++)
-            for(j=1;j<ny-1;j++) {
+
+    if (game == 0)
+    { // Use Random input
+        for (i = 1; i < nx - 1; i++)
+            for (j = 1; j < ny - 1; j++)
+            {
                 currWorld[i][j] = (real_rand() < prob);
                 population += currWorld[i][j];
             }
     }
-    else if (game == 1){ //  Block, still life
+    else if (game == 1)
+    { //  Block, still life
         printf("2x2 Block, still life\n");
-        int nx2 = nx/2;
-        int ny2 = ny/2;
-        currWorld[nx2+1][ny2+1] = currWorld[nx2][ny2+1] = currWorld[nx2+1][ny2] = currWorld[nx2][ny2] = 1;
+        int nx2 = nx / 2;
+        int ny2 = ny / 2;
+        currWorld[nx2 + 1][ny2 + 1] = currWorld[nx2][ny2 + 1] = currWorld[nx2 + 1][ny2] = currWorld[nx2][ny2] = 1;
         population = 4;
     }
-    else if (game == 2){ //  Glider (spaceship)
+    else if (game == 2)
+    { //  Glider (spaceship)
         printf("Glider (spaceship)\n");
         // Your code codes here
     }
-    else{
-        printf("Unknown game %d\n",game);
+    else
+    {
+        printf("Unknown game %d\n", game);
         exit(-1);
     }
-    
-    printf("probability: %f\n",prob);
+
+    printf("probability: %f\n", prob);
     printf("Random # generator seed: %d\n", rs);
 
     /* Plot the initial data */
-    if(!disable_display)
-      MeshPlot(0,nx,ny,currWorld);
-    
+    if (!disable_display)
+        MeshPlot(0, nx, ny, currWorld);
+
     /* Perform updates for maxiter iterations */
     double t0 = getTime();
     int t;
 
-    for(t=0;t<maxiter && population;t++)
+    for (t = 0; t < maxiter && population; t++)
     {
         /* Use currWorld to compute the updates and store it in nextWorld */
-      population = 0;
-      for(i=1;i<nx-1;i++)
-            for(j=1;j<ny-1;j++) {
-	      int nn = currWorld[i+1][j] + currWorld[i-1][j] + 
-		currWorld[i][j+1] + currWorld[i][j-1] + 
-		currWorld[i+1][j+1] + currWorld[i-1][j-1] + 
-		currWorld[i-1][j+1] + currWorld[i+1][j-1];
-	      
-	      nextWorld[i][j] = currWorld[i][j] ? (nn == 2 || nn == 3) : (nn == 3);
-	      population += nextWorld[i][j];
+        population = 0;
+        for (i = 1; i < nx - 1; i++)
+            for (j = 1; j < ny - 1; j++)
+            {
+                int nn = currWorld[i + 1][j] + currWorld[i - 1][j] +
+                         currWorld[i][j + 1] + currWorld[i][j - 1] +
+                         currWorld[i + 1][j + 1] + currWorld[i - 1][j - 1] +
+                         currWorld[i - 1][j + 1] + currWorld[i + 1][j - 1];
+
+                nextWorld[i][j] = currWorld[i][j] ? (nn == 2 || nn == 3) : (nn == 3);
+                population += nextWorld[i][j];
             }
-      
-      
-      /* Pointer Swap : nextWorld <-> currWorld */
-      tmesh = nextWorld;
-      nextWorld = currWorld;
-      currWorld = tmesh;
-      
-      /* Start the new plot */
-      if(!disable_display)
-	MeshPlot(t,nx,ny,currWorld);
-      
-      if (s_step){
-	printf("Finished with step %d\n",t);
-	printf("Press enter to continue.\n");
-	getchar();
-      }
-      
+
+        /* Pointer Swap : nextWorld <-> currWorld */
+        tmesh = nextWorld;
+        nextWorld = currWorld;
+        currWorld = tmesh;
+
+        /* Start the new plot */
+        if (!disable_display)
+            MeshPlot(t, nx, ny, currWorld);
+
+        if (s_step)
+        {
+            printf("Finished with step %d\n", t);
+            printf("Press enter to continue.\n");
+            getchar();
+        }
     }
-    double t1 = getTime(); 
-    printf("Running time for the iterations: %f sec.\n",t1-t0);
+
+    double t1 = getTime();
+    printf("Running time for the iterations: %f sec.\n", t1 - t0);
     printf("Press enter to end.\n");
     getchar();
-    
-    if(gnu != NULL)
-      pclose(gnu);
-        
+
+    if (gnu != NULL)
+        pclose(gnu);
+
     /* Free resources */
     free(nextWorld);
     free(currWorld);
 
-    return(0);
+    return (0);
 }
