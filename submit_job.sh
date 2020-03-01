084d17f20c2a3ddd3c8e753ca4a80cb68d2033c6
DorukTaneli <dtaneli16@ku.edu.tr>
2020-03-01 20:40:25 +0300

    add experiments

--- submit_job.sh
+++ submit_job.sh
@@ -32,12 +32,64 @@ echo "Serial version..."
 ./life -n 2000 -i 500 -p 0.2 -d
 
 #parallel version
-#echo "Parallel version with 32 threads"
-#export OMP_NUM_THREADS=32 
-#export KMP_AFFINITY=verbose,granularity=fine,compact
-#./life_parallel
+#first experiment
+echo "Parallel version with 2 threads"
+export OMP_NUM_THREADS=2
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./life -n 2000 -i 500 -p 0.2 -d -t 2
 
-#echo "Parallel version with 16 threads"
-#export OMP_NUM_THREADS=16
-#export KMP_AFFINITY=verbose,granularity=fine,compact
-#./life_parallel
+echo "Parallel version with 4 threads"
+export OMP_NUM_THREADS=4
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./life -n 2000 -i 500 -p 0.2 -d -t 4
+
+echo "Parallel version with 8 threads"
+export OMP_NUM_THREADS=8
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./life -n 2000 -i 500 -p 0.2 -d -t 8
+
+echo "Parallel version with 16 threads"
+export OMP_NUM_THREADS=16
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./life -n 2000 -i 500 -p 0.2 -d -t 16
+
+echo "Parallel version with 32 threads"
+export OMP_NUM_THREADS=32 
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./life -n 2000 -i 500 -p 0.2 -d -t 32
+
+#second experiment
+echo "n 2000 with 16 threads"
+export OMP_NUM_THREADS=16
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./life -n 2000 -i 500 -p 0.2 -d -t 16
+
+echo "n 4000 with 16 threads"
+export OMP_NUM_THREADS=16
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./life -n 4000 -i 500 -p 0.2 -d -t 16
+
+echo "n 6000 with 16 threads"
+export OMP_NUM_THREADS=16
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./life -n 6000 -i 500 -p 0.2 -d -t 16
+
+echo "n 8000 with 16 threads"
+export OMP_NUM_THREADS=16
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./life -n 8000 -i 500 -p 0.2 -d -t 16
+
+echo "n 10000 with 16 threads"
+export OMP_NUM_THREADS=16
+export KMP_AFFINITY=verbose,granularity=fine,compact
+export OMP_NESTED=true
+./life -n 10000 -i 500 -p 0.2 -d -t 16
